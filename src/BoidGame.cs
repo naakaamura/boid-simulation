7c1208017264a2fee9cf5b439da524adb54edb28
jv.naka <joaovtnakamura@gmail.com>
2022-07-05 20:15:02 -0300

    Base game

---
+++ src/BoidGame.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace BoidSimulation
+{
+    public sealed class BoidGame : Game
+    {
+        public GraphicsDeviceManager Graphics { get; private set; }
+        public SpriteBatch SpriteBatch { get; private set; }
+
+        public BoidGame() : base()
+        {
+            Graphics = new(this);
+
+            IsMouseVisible = true;
+        }
+
+        protected override void LoadContent()
+        {
+            SpriteBatch = new SpriteBatch(GraphicsDevice);
+
+            base.LoadContent();
+        }
+    }
+}
